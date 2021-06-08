80a39f68d36573b03f9b8a177b8f3e1a0af9cf84
altafshaikh <iamaltafshaikh07@gmail.com>
2021-06-08 15:46:48 +0530

    feat: :sparkles: Created Schema File

---
+++ schema.js
@@ -0,0 +1,51 @@
+import { buildSchema } from "graphql";
+
+const schema = buildSchema(`
+    type Course {
+        id: ID
+        courseName: String
+        category: String
+        price: Int
+        language: String
+        email: String
+        stack: Stack
+        teachingAssists: [TeacinghAssist]
+    }
+
+    type TeacinghAssist {
+        firstName: String
+        lastName: String
+        experience: Int
+    }
+
+    enum Stack {
+        WEB
+        MOBILE
+        OTHER
+    }
+
+    type Query {
+        getCourse(id: ID): Course
+    }
+
+    input CourseInput {
+        id: ID
+        courseName: String!
+        category: String
+        price: Int!
+        language: String
+        email: String
+        stack: Stack
+        teachingAssists: [TeacinghAssistInput]!
+    }
+
+    input TeacinghAssistInput {
+        firstName: String
+        lastName: String
+        experience: Int
+    }
+
+    type Mutation {
+        createCourse(input CourseInput): Course
+    }
+`);
